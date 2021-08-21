72d560a589f4bc83593eebab109e51a7b384eef5
Eduardo Bravo <eduardobravopires@gmail.com>
2021-08-21 09:37:11 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+
+import styles from '../styles/pages/SignIn.module.scss'
+
+const SignIn: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <form>
+        
+      </form>
+    </div>
+  )
+}
+
+export default SignIn
