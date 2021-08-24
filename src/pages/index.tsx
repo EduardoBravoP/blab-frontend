7130aef8dc80b3a8984bdf204f95fb3bdce08c8b
Eduardo Bravo <eduardobravopires@gmail.com>
2021-08-24 20:08:16 -0300

    feat: Started to work on login page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,32 @@
-import type { NextPage } from 'next'
+import { NextPage } from "next";
+import Image from "next/image";
+import Input from "../components/Input";
 
-import styles from '../styles/pages/SignIn.module.scss'
+import styles from "../styles/pages/SignIn.module.scss";
 
 const SignIn: NextPage = () => {
   return (
     <div className={styles.container}>
+      <aside>
+        <Image
+          src="/Vector.png"
+          alt="Imagem de fundo"
+          height={700}
+          width={700}
+        />
+      </aside>
+
       <form>
-        
+        <h1>Entrar</h1>
+
+        <Input label="Nome" />
+
+        <Input label="E-mail" />
+
+        <Input label="Avatar (URL)" />
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default SignIn
+export default SignIn;
